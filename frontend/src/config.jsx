53ea7dc5629dc01245863bde2501d8a3eaa18e98
Akshay9522-cell <akshaynamdev963@gmail.com>
2025-03-25 08:52:06 +0530

    final

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,6 +1,8 @@
 
- const URL='http://localhost:8000/bank'
-//const URL='https://placement-project-1-2.onrender.com'
+ //const URL='http://localhost:8000/bank'
+const URL='https://placement-project-1-2.onrender.com/bank'
+//const URL='https://placement-project-1-5.onrender.com/bank'
+
 
 
 export default URL
